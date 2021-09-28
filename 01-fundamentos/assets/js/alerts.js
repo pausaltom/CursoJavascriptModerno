d37266aab6455eb3c15e40e1397d9d1932cf718a
Pau Salto <pausaltom@gmail.com>
2021-09-28 20:41:29 +0200

    primer commit

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,9 @@
+
+// alert('Hola que tal '+ window.miNombre)
+
+// let respTieneNombre = prompt('¿Cual es tu nombre?','Sin nombre');
+// console.log(respTieneNombre)
+
+// const respTieneNombre2 = confirm('Esta seguro de borrar esto?');
+// console.log(respTieneNombre2)
+
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,25 @@
+// Hola Mundo
+//console.log('hola Mundo');
+
+// Tipos de variables, var, let , const
+// Siempre utilizar let o const, intentar no usar var pq es peligroso
+// let a = 10,
+//     b = 20,
+//     c= 'Hola ',
+//     d = 'SpiderMan',
+//     x = a + b;
+
+// const saludo= c+ d;
+
+// console.log(x)
+// console.warn(x)
+// console.info(x)
+// console.error(x)
+
+// console.log('a: ' + a);
+// console.log({ a });
+// console.log(saludo);
+
+// console.table({a, b, c, d, saludo})
+
+
